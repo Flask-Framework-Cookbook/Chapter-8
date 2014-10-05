734ce3ffa2313c96eaf71cc4d3047ea04d9c68ac
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-10-06 01:00:19 +0530

    Chapter 6 - Recipe 'Authenticating using Flask-Login extension'

--- my_app/auth/models.py
+++ my_app/auth/models.py
@@ -17,6 +17,18 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
 
 class RegistrationForm(Form):
     username = TextField('Username', [InputRequired()])
